e34467f4210fc66d426679ec1d4c10436914c9fc
Aulon Mujaj <aulon.mujaj@gmail.com>
2021-03-08 14:47:02 +0100

    add vue-resize

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResize from 'vue-resize';
 
 Vue.config.productionTip = false
+
+Vue.use(VueResize);
 
 new Vue({
   render: h => h(App),
